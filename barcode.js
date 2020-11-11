3d2d561da6d40fbc99363d045742ea52436cb4a4
njvillavicencio <njvillavicencio@uc.cl>
2020-11-10 22:51:45 -0300

    Update barcode.js

--- barcode.js
+++ barcode.js
@@ -1,6 +1,4 @@
 $(document).ready(function(){
-
-
     $("body").on('click',"#escanear", function(){
         getEscaner();
         document.getElementById('escaner').style.display = 'block';
@@ -22,8 +20,8 @@ $(document).ready(function(){
        var cantidad = document.getElementById("cantidad").value;
        var validacion = validarStock(codigo, parseInt(cantidad));
        if (validacion) {
-// pormientras           escribirBaseDatos(codigo,descripcion,parseInt(cantidad));       
-// pormientras           escribirDatos(idArchivo, nombreHoja+"!A1:E1",[codigo,descripcion,cantidad,0,0]);
+           escribirBaseDatos(codigo,descripcion,parseInt(cantidad));       
+           escribirDatos(idArchivo, nombreHoja+"!A1:E1",[codigo,descripcion,cantidad,0,0]);
            document.getElementById("datos").reset();
            escanear();
        }
@@ -31,18 +29,182 @@ $(document).ready(function(){
            alert("Este artículo no tiene stock suficiente.");           
        }
       });  
-    
-
 });
 
-function escanear(){
-        getEscaner();
-            document.getElementById('escaner').style.display = 'block';
-        document.getElementById('escanear').style.display = 'none';
-        document.getElementById('cerrar').style.display = 'block';
-        document.getElementById('datos').style.display = 'none';
-}
+// programa
+    function obtenerDatosApp() {
+        gapi.client.drive.files.list({
+          spaces: 'appDataFolder',
+          fields: 'files(id,name)'
+        }).then(function(response) {
+          var archivos = response.result.files;
+          if (archivos && archivos.length > 0) {
+            var archivo = archivos[0];
+            obtenerArchivo(archivo.name);
+	        idArchivo=archivo.name;
+          }    
+         else {
+		    crearArchivo();
+	      }
+        });
+      }
+      
+      function crearDatosApp(fileId) {
+        gapi.client.drive.files.create({
+          resource: {
+            name: fileId,
+            parents: ['appDataFolder']
+          },
+          fields: 'id'
+        }).then(function (data) {obtenerDatosApp();});
+        
+      };
+   function obtenerArchivo(idArchivo) {
+ 	var request = gapi.client.sheets.spreadsheets.get({spreadsheetId: idArchivo});
+        request.then(function(response) {
+		data= response.result.sheets;
+		var encontrarHoja=-1;
+		//var nombreHoja = "";
+		for (var i=0; i < data.length; i++)
+		{
+			if (data[i].properties.sheetId==0){			
+				encontrarHoja=0;
+				nombreHoja=data[i].properties.title;
+			}
+		}
+		if (encontrarHoja==-1) {
+			crearHoja(idArchivo);	
+		}
+		else {
+			leerDatos(idArchivo,nombreHoja+"!A:E");	
+		}
 
+      	}, function(reason) {
+		crearArchivo();
+       		console.error('error: ' + reason.result.error.message);
+      	}); }
+	      
+	      
+    function crearHoja(idArchivo) {
+      var params = {spreadsheetId: idArchivo};
+      var batchUpdateSpreadsheetRequestBody = {
+        requests: [{"addSheet": {"properties": {"sheetId": 0,"title": "Datos Escaneados"}}}], 
+      };
+      var request = gapi.client.sheets.spreadsheets.batchUpdate(params, batchUpdateSpreadsheetRequestBody);
+      request.then(function(response) {
+        console.log(response.result);
+      }, function(reason) {
+        console.error('error: ' + reason.result.error.message);
+      });
+    }   
+	      
+    function crearArchivo() {
+      var spreadsheetBody = {"properties": {"title": "BarCodeScanner"},"sheets": [{"properties": {"sheetId": 0,"title": "Datos Escaneados"}}]}
+      var request = gapi.client.sheets.spreadsheets.create({}, spreadsheetBody);
+      request.then(function(response) {
+        crearDatosApp(response.result.spreadsheetId);
+      }, function(reason) {
+        console.error('error: ' + reason.result.error.message);
+      });
+    }
+
+    function leerDatos(idArchivo, rango) {
+	var params = {spreadsheetId: idArchivo,  range: rango};	   
+	var request = gapi.client.sheets.spreadsheets.values.get(params);  
+	request.then(function(response) {
+        	datos= response.result.values;  
+		if (!(datos === undefined)) {
+			for (var i = 0; i < datos.length; i++) {
+				var codigo= datos[i][0];
+				var descripcion = datos[i][1];
+				var cantidad = parseInt(datos[i][2]);
+				if (!(codigo in baseDatos)){ 
+					baseDatos[codigo]=descripcion
+					balanceStock[codigo]=cantidad;
+				}
+				else {
+					balanceStock[codigo]=balanceStock[codigo]+cantidad;
+				}
+				
+			}
+         	}
+         }, function(reason) {
+             console.error('error: ' + reason.result.error.message);
+         });	    
+   }	      
+    function validarDatos(){
+	var largoDescripcion = document.getElementById('descripcion').value.length;
+	var largoCantidad = document.getElementById('cantidad').value.length;
+	var cantidad = parseInt(document.getElementById('cantidad').value);
+        if(largoDescripcion > 0 && largoCantidad>0 && cantidad!=0)  { 
+            document.getElementById('guardar').disabled = false; 
+        } else { 
+            document.getElementById('guardar').disabled = true;
+        }
+    }
+   function leerBaseDatos(codigo) {
+		var descripcion="";
+		if (codigo in baseDatos){ 
+                	descripcion = baseDatos[codigo];		
+		}
+                document.getElementById('escaner').style.display = 'none';
+                document.getElementById('datos').style.display = 'block';
+                document.getElementById('escanear').style.display = 'block';
+                document.getElementById("codigo").value = codigo;
+                document.getElementById("codigo").disabled = true;
+		document.getElementById("descripcion").value = descripcion;
+		if (descripcion.length>0){
+                document.getElementById("descripcion").disabled = true;  
+		}
+   }
+		
+   function escribirBaseDatos(codigo, descripcion, cantidad) {
+		if (!(codigo in baseDatos)){ 
+                	baseDatos[codigo]=descripcion;	
+			balanceStock[codigo]=cantidad;
+		} 
+	   	else {
+			balanceStock[codigo]=balanceStock[codigo]+cantidad;
+			console.log(balanceStock[codigo]);
+		}
+   }
+	
+	      
+	      function validarStock(codigo,cantidad) {
+		if (cantidad>0){
+			return true;
+		}
+		else if (balanceStock[codigo]+cantidad>=0){
+			return true;
+		}
+		else {
+			return false;	
+		}
+		      
+	      }
+   function escribirDatos(fileId, range, information) {
+      var params = {
+        spreadsheetId: fileId,
+        range: range,
+        valueInputOption: 'USER_ENTERED',
+        insertDataOption: 'INSERT_ROWS'};
+      var valueRangeBody = {'values': [information],'majorDimension':'ROWS'};
+      var request = gapi.client.sheets.spreadsheets.values.append(params, valueRangeBody);
+      request.then(function(response) {
+      }, function(reason) {
+        console.error('error: ' + reason.result.error.message);
+      });
+    }
+
+//-----escaner
+
+        function escanear(){
+                getEscaner();
+                document.getElementById('escaner').style.display = 'block';
+                document.getElementById('escanear').style.display = 'none';
+                document.getElementById('cerrar').style.display = 'block';
+                document.getElementById('datos').style.display = 'none';
+        }
 
           function getEscaner($btn){
             let detectionHash={};
@@ -52,23 +214,12 @@ function escanear(){
                 var median;
                 for (var i=1; i<=result.codeResult.decodedCodes.length-1;i++){
                     codeErrors.push(result.codeResult.decodedCodes[i].error);
-//                    if (result.codeResult.decodedCodes[i].error>0.1){
-//                        validCode=false;
-                        
-//                    }
                 }              
                 codeErrors.sort(function(a, b){return a-b});
-//                 if (codeErrors.length % 2 == 0) {
-//                     median=codeErrors[codeErrors.length/2];                    
-//                 }
-//                 else {
-                     //median=(codeErrors[Math.floor(codeErrors.length/2)-1]+codeErrors[Math.floor(codeErrors.length/2)+1])/2;
                 median=codeErrors[Math.floor(codeErrors.length/2)+1];
-//                 }
                  if (median>0.1) {
                      validCode=false;
-                 }
-                
+                 }            
                 if (validCode) {
                     if (detectionHash[result.codeResult.code]>=1){
                         detectionHash[result.codeResult.code]=detectionHash[result.codeResult.code]+1;
@@ -82,7 +233,7 @@ function escanear(){
                     detectionHash = {};
                     var last_code = result.codeResult.code;
                     Quagga.stop();
-// por mientras                    leerBaseDatos(last_code);
+                    leerBaseDatos(last_code);
                 }
             },function(reason) {
        		console.error('error: ' + reason.result.error.message);
