e9be4982c118e26d6f2f686e462bb783b6ce03df
njvillavicencio <njvillavicencio@uc.cl>
2020-11-12 07:08:36 -0300

    Update barcode.js

--- barcode.js
+++ barcode.js
@@ -248,7 +248,12 @@ $(document).ready(function(){
                 },
                 decoder: {
                     readers : ["code_128_reader","ean_reader","ean_8_reader","code_39_reader","code_39_vin_reader","codabar_reader","upc_reader","upc_e_reader","i2of5_reader","2of5_reader","code_93_reader"]
-                }
+                },
+constraints: {
+            width: window.innerWidth,
+            height: window.innerHeight,
+            facing: "environment" // or user
+        }
               },function(err) {
                   if (err) { console.log(err); return }
                   Quagga.initialized = true;
