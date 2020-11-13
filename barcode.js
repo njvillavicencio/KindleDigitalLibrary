1e86e8698d56c191837afdbadbed69e8ad6a1e14
njvillavicencio <njvillavicencio@uc.cl>
2020-11-12 23:05:31 -0300

    Update barcode.js

--- barcode.js
+++ barcode.js
@@ -246,7 +246,11 @@ $(document).ready(function(){
                   type : "LiveStream",
                   numOfWorkers: navigator.hardwareConcurrency,
                   target: document.querySelector('#escaner')  
-                },locate: true,
+                },locate: true,            
+		locator: {
+                patchSize: "medium",
+                halfSample: true
+            },
                 decoder: {
                     readers : ["code_128_reader","ean_reader","ean_8_reader","code_39_reader","code_39_vin_reader","codabar_reader","upc_reader","upc_e_reader","i2of5_reader","2of5_reader","code_93_reader"]
                 }
